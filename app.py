88d061fb412080fe028216821fecebd4bfa30c62
moonymercury <148629739+moonymercury@users.noreply.github.com>
2023-10-22 10:08:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -67,13 +67,18 @@ def handle_message(event):
     # 例如，儲存使用者的 todo list
 
     # add
-    if str(msg).strip() == "現在幾點" or str(msg).strip() == "抽" or str(msg).strip() == "去哪裡":
+    if str(msg).strip() == "集點" or str(msg).strip() == "抽" or str(msg).strip() == "去哪裡" or str(msg).strip() == "現在集點":
         
-        collection = db.get_collection("spots")  # 替換成你的集合名稱
+        collection = db.get_collection("spots")
+        collection1 = db.get_collection("Art")
+        # 替換成你的集合名稱
         # 檢索所有資料
         cursor = collection.find()
+        cursor1 = collection1.find()
         # 將檢索到的資料轉換為 Python 列表
         data = list(cursor)
+        data1 = list(cursor1)
+        data = data+data1
         print(data) #test
         # 使用時間來生成種子
         random.seed(int(time.time()))
@@ -96,6 +101,24 @@ def handle_message(event):
         print(checkbox_template)
         # 回覆使用者訊息，使用 Buttons Template 提供選項
         line_bot_api.reply_message(event.reply_token, checkbox_template) """
+    elif str(msg).strip()=='飯點':
+        collection = db.get_collection("spots")
+        cursor = collection.find()
+        data = list(cursor)
+         # 使用時間來生成種子
+        random.seed(int(time.time()))
+        # 隨機選擇一個項目
+        random_item = random.choice(data)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"想不到要去哪裡嗎?\n建議您可以前往「{random_item['name']}」品嚐美食!\n\n{random_item['link']}\n\n另外，用餐並留下評論便可以集點喔~\n快來試試吧！"))
+    elif str(msg).strip() == '藝點':
+        collection = db.get_collection("Art")
+        cursor = collection.find()
+        data = list(cursor)
+         # 使用時間來生成種子
+        random.seed(int(time.time()))
+        # 隨機選擇一個項目
+        random_item = random.choice(data)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"想不到要去哪裡嗎?\n建議您可以前往「{random_item['name']}」品嚐美食!\n\n{random_item['link']}\n\n另外，用餐並留下評論便可以集點喔~\n快來試試吧！"))
     elif str(msg).strip() == "功能" or  str(msg).strip() == "打卡" or str(msg).strip() == "卡景點" or str(msg).strip() == "集點" or str(msg).strip() == "集點卡" or str(msg).strip() == "現在集點" or  str(msg).strip() == "探索地圖" or  str(msg).strip() == "地圖":
         pass
     else:
